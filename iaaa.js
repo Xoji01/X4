23913a77d0b2c4d3fa091e08c6c29a97047f77c5
Xoji01 <135627056+Xoji01@users.noreply.github.com>
2023-06-26 18:20:59 +0500

    Add files via upload

--- iaaa.js
+++ iaaa.js
@@ -1,5 +1,5 @@
-let arr = [{a: {price: 20}},{a: {price: 35}},{a: {price: 44}}]
-let total = 0
+let arr = [[{a: {price: 20}}],[{a: {price: 35}}],[{a: {price: 44}}]]
+let total = arr[0][0].a.price+arr[1][0].a.price+arr[2][0].a.price
 console.log(
-    arr[0].a.price+arr[1].a.price+arr[2].a.price
+    total
 )
